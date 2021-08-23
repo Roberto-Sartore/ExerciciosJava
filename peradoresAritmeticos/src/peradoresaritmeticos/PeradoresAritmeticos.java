c94de0a974a9b5c863c0193268f3ad5dd1c4d395
Roberto Sartore <eng.robertosartore@gmail.com>
2021-08-23 08:51:59 -0300

    exercicios feitos durante o curso de java

---
+++ peradoresAritmeticos/src/peradoresaritmeticos/PeradoresAritmeticos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peradoresaritmeticos;
+
+/**
+ *
+ * @author Pc
+ */
+public class PeradoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1=3;
+        int n2=5;
+        float m=(n1+n2)/2;
+        System.out.println("A media entre "+n1+ " e "+n2+ " é igual "+m);
+      
+        int numero = 5;
+        int valor = 5 + ++numero;
+        System.out.println(valor);
+        
+        int x = 4;
+        x += 2;
+        System.out.println(x);
+        
+    }
+    
+}
